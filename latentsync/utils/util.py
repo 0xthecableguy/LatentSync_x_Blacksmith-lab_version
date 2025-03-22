e5587032cdee122ff5f7e854bcd6fa1df3d7b4e8
0xthecableguy <0xthecableguy@proton.me>
2025-03-23 01:15:23 +0300

    Testing optimized workflow #12

--- latentsync/utils/util.py
+++ latentsync/utils/util.py
@@ -167,7 +167,7 @@ def read_audio(audio_path: str, audio_sample_rate: int = 16000):
 
 def write_video(batch_output_path, frames, fps=25):
     height, width = frames[0].shape[:2]
-    fourcc = cv2.VideoWriter_fourcc(*'H264')
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
     out = cv2.VideoWriter(batch_output_path, fourcc, fps, (width, height), isColor=True)
     for frame in frames:
         frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
